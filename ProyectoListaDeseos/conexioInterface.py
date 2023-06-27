22f95ede6af08067e7d8de9a0bfffe1445f225df
Carlos Calvo <89465394+rec0d3@users.noreply.github.com>
2023-06-27 18:49:53 -0400

    Update conexiónInterface.py

--- ProyectoListaDeseos/conexioInterface.py
+++ ProyectoListaDeseos/conexioInterface.py
@@ -1,3 +1,28 @@
 import psycopg2 # Importamos la base de datos, usaremos PostgresSQL
 class ConexionDB:#Definimos la clase
-    # Establecemos la conexion con la base de datos
+ 
+# Establecemos la conexion con la base de datos
+    def init(self):
+            self.conexion = psycopg2.connect(
+                host="localhost",
+                database="proyecto_Python",
+                user="postgres",
+                password="admin"
+            )
+            self.crear_tabla()# LLamamos al metodo crear tabla
+    
+    def crear_tabla(self):
+        # Con este metodo creamos la tabla 'deseos' si no existe
+        # Utilizara los siguientes campos: id, producto, cantidad, precio
+        cursor = self.conexion.cursor()
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS deseos (
+                id SERIAL PRIMARY KEY,
+                producto TEXT,
+                cantidad INTEGER,
+                precio REAL
+            )
+            """
+        )
+        self.conexion.commit()
